fdf45e66a99190142e28b0f21b3357f6a96f11ec
maylearnweb <may.learnweb@gmail.com>
2025-12-22 12:00:01 +0100

    Fix deployment: Enable SSL for Postgres, update frontend build auth

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0',
+  apiHost: 'http://demo-app-env.eba-y5rx34us.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
