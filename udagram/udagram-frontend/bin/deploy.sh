30961604391a324415c047650e559ffd42dd0a5d
maylearnweb <may.learnweb@gmail.com>
2025-12-23 10:01:30 +0100

    Fix: Update deploy script to use environment variable

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,2 +1,2 @@
-aws s3 cp --recursive ./www s3://bucket-udacity-app/
-aws s3 cp --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://bucket-udacity-app/
+aws s3 cp --recursive ./www s3://$AWS_BUCKET/
+aws s3 cp --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://$AWS_BUCKET/
